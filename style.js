04e52d177361ea642f4769c8bfd52f5413feb008
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-07 19:35:29 -0600

    Update style.js

--- style.js
+++ style.js
@@ -1,3 +1,5 @@
 function getComputerChoice(){
-    const 
+    const choices = ['rock','paper','scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
